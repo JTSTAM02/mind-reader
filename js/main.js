7b2aa91492e1670a25b73d0bc062faf9fc4f6d68
Tyler Stamm <tyler.stamm1313@gmail.com>
2023-06-07 20:10:06 +0000

    Getting there!

--- js/main.js
+++ js/main.js
@@ -1,22 +1,37 @@
 var currentCard = 0;
 
-    const button = document.querySelector(".card-text");
-    const text = document.getElementById("text");
+    const button1 = document.getElementById("button1");
+    const button2 = document.getElementById("button2");
+    const text = document.getElementById("newContent");
+    const additionalContent = document.getElementById("additionalContent");
+    const additionalText = document.getElementById("additionalText");
+
     const mindReader = {
-    "pages" : ["I can read your mind.", "Pick a number from 11-99", "Add both digits together to get a new number", "Subtract your new number from the original number", "do later", "&" ],
+    "text" : ["I can read your mind.", "Pick a number from 11-99", "Add both digits together to get a new number", "Subtract your new number from the original number", "do later", "&" ],
+    "pages" : [""],
     "render" : function() {
-            var output = this.pages[currentCard];
-            text.textContent = mindReader.pages[currentCard];
-            document.querySelector(".card-title").textContent = output;
+            var output = this.text[currentCard]; //
+            document.querySelector(".card-title").textContent = output; //
+
+
+        if(currentCard !== 0) {
+            button1.textContent = "Next";
+            additionalContent.style.display = "block";
+            additionalText.style.display = "none";
+        }
     }
 };
 
 function buttonClick() {
-    currentCard = currentCard + 1 % mindReader.pages.length;
+    currentCard = currentCard + 1 ; //
     mindReader.render();
 }
-
-button.addEventListener("click", buttonClick);
-
+button1.classList.add("btn-circle");
+button2.classList.add("btn-circle");
+button1.addEventListener("click", buttonClick);
+button2.addEventListener("click", function() {
+    currentCard = 0;
+    console.log(currentCard);
+    mindReader.render();
+});
 mindReader.render();
-
